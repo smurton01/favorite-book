7067bca154a79ed69ada7f35834e481c59128652
Saya Murton <saya.murton@gmail.com>
2017-09-01 11:27:18 -0600

    Copy & paste
    -added `updateView` method to "BookFormTableViewController"
    -filled in `saveButtonTapped` method
    -both copied & pasted from "BookFormViewController"

--- FavoriteBook/BookFormTableViewController.swift
+++ FavoriteBook/BookFormTableViewController.swift
@@ -14,17 +14,34 @@ class BookFormTableViewController: UITableViewController {
     @IBOutlet weak var authorTextField: UITextField!
     @IBOutlet weak var genreTextField: UITextField!
     @IBOutlet weak var lengthTextField: UITextField!
-    @IBOutlet weak var saveButton: UIButton!
+
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        updateView()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
-
+    
+    func updateView() {
+        guard let book = book else {return}
+        
+        titleTextField.text = book.title
+        authorTextField.text = book.author
+        genreTextField.text = book.genre
+        lengthTextField.text = book.length
+    }
+    
+    @IBAction func saveButtonTapped(_ sender: Any) {
+        guard let title = titleTextField.text,
+            let author = authorTextField.text,
+            let genre = genreTextField.text,
+            let length = lengthTextField.text else {return}
+            
+            book = Book(title: title, author: author, genre: genre, length: length)
+            performSegue(withIdentifier: PropertyKeys.unwind, sender: self)
+        }
 }
+
+
