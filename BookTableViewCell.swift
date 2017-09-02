cbb5ceb7e1923e5152411564b312f45b34c7dbd5
Saya Murton <saya.murton@gmail.com>
2017-09-01 18:27:47 -0600

    Added "BookTableViewCell"
    -added UITableViewCell "BookTableViewCell"

---
+++ BookTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  BookTableViewCell.swift
+//  FavoriteBooks
+//
+//  Created by Saya Murton on 9/1/17.
+//
+//
+
+import UIKit
+
+class BookTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
